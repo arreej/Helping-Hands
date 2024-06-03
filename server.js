4167ddcc981a7ecb4f1598598c771154550165ad
apple <areej.fatima2k@gmail.com>
2024-06-04 01:10:11 +0500

    port-changes

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
-    console.log('Server is running on port', PORT)
-})
+const port = process.env.PORT || 5000; // Use the environment variable PORT or default to 5000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
